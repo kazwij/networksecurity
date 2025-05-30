db5bfd552b9feb8ea05d54738336413789e0e208
Kasun Perumbuli <kasun.worksite@gmail.com>
2025-05-30 13:53:57 +0100

    mlflow tracking added to train and test model

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -42,9 +42,8 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
     
-    #def track_mlflow(self,best_model,classificationmetric):
-
-        '''with mlflow.start_run():
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
             f1_score = classificationmetric.f1_score
             precision_score = classificationmetric.precision_score
             recall_score=classificationmetric.recall_score
@@ -56,7 +55,6 @@ class ModelTrainer:
             mlflow.sklearn.log_model(best_model,"model")
             
     
-    '''    
     def train_model(self,X_train,y_train,X_test,y_test):
         try:
             models = {
@@ -111,7 +109,8 @@ class ModelTrainer:
             classification_train_metric = get_classification_score(y_true=y_train,y_pred=y_train_pred)
             
             ## Track the mlflow
-            
+            self.track_mlflow(best_model,classification_train_metric)
+             
             y_test_pred  = best_model.predict(X_test)
             
             classification_test_metric = get_classification_score(y_true=y_test,y_pred=y_test_pred) 
